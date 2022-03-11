9992c1a6e9a0c4327e7ca1d7f1fb7d13056af0e4
Jess <jssanji03@gmail.com>
2022-03-11 16:06:22 +0800

    修改 優化程式碼

--- src/index.js
+++ src/index.js
@@ -6,21 +6,9 @@ import "bootstrap";
 import './scss/index.scss';
 
 //import js
-// import './js/drag2'
 import { dragTest } from './js/drag2';
-
 dragTest()
-// import * as hello from './js/drag2'
-// hello.test()
 
-// hello.allowDrop()
-// hello.drag()
-// display();
-// myModule.allowDrop();
-// myModule.drag();
-// myModule.drop_handler();
-// myModule.drop();
-// myModule.TwoDecimal();
 
 if (module.hot) {
     module.hot.accept();
